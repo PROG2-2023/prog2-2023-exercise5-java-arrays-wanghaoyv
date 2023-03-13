c8661cd167b756cc392750c5582eefc6fb8c7735
wanghaoyv <126221103+wanghaoyv@users.noreply.github.com>
2023-03-13 09:01:34 +0800

    Update App.java

--- src/main/java/prog2/exercise5/flight/booking/system/App.java
+++ src/main/java/prog2/exercise5/flight/booking/system/App.java
@@ -1,13 +1,1 @@
-package prog2.exercise5.flight.booking.system;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
